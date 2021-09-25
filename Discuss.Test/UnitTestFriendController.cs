a043e22255fd9e77b417c4af148738c1ccdc2cfe
szyma <szymaborys@gmail.com>
2021-09-25 19:49:34 +0200

    Add test FriendService

--- Discuss.Test/UnitTestFriendController.cs
+++ Discuss.Test/UnitTestFriendController.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Discuss.Domain.Models.Entities;
 using Discuss.Infrastructure.Services;
@@ -12,16 +13,17 @@ namespace Discuss.Test
     public class UnitTestFriendController
     {
         private FriendService _friendService;
+        private Dictionary<string, IEnumerable<string>> _friendly;
         private DbContextOptions<ApplicationDbContext> _dbContextOptions = new DbContextOptionsBuilder<ApplicationDbContext>().UseInMemoryDatabase(databaseName: "UnitTestFriendController").Options;
-
+        
         private void InitDb()
         {
             using var dbContext = new ApplicationDbContext(_dbContextOptions);
             var users = new List<User>()
             {
-                new User{ Id = "0", Email = "1@gmail.com", Login = "LoginA"},
-                new User{ Id = "1", Email = "2@gmail.com", Login = "LoginB"},
-                new User{ Id = "2", Email = "3@gmail.com", Login = "LoginC"},
+                new User{ Id = "0", Email = "1@gmail.com", Login = "LoginA", Status = true},
+                new User{ Id = "1", Email = "2@gmail.com", Login = "LoginB", Status = false},
+                new User{ Id = "2", Email = "3@gmail.com", Login = "LoginC", Status = false},
             };
 
             var friends = new List<Friend>()
@@ -31,6 +33,13 @@ namespace Discuss.Test
                 new Friend() {UserId = "1",FriendlyId = "2"},
                 new Friend() {UserId = "2",FriendlyId = "1"},
             };
+
+            _friendly = new Dictionary<string, IEnumerable<string>>()
+            {
+                {"0",new List<string>(){"1"}},
+                {"1",new List<string>(){"0","2"}},
+                {"2",new List<string>(){"1"}},
+            };
             
             dbContext.AddRange(users);
             dbContext.AddRange(friends);
@@ -38,7 +47,7 @@ namespace Discuss.Test
             dbContext.SaveChanges();
         }
         
-        [SetUp]
+        [OneTimeSetUp]
         public void Setup()
         {
             _friendService = new FriendService(new ApplicationDbContext(_dbContextOptions));
@@ -46,14 +55,46 @@ namespace Discuss.Test
             InitDb();
         }
 
+        [TestCase("0")]
         [TestCase("1")]
-        public async Task GetFriend_FindFriend_User(string id)
+        [TestCase("2")]
+        public async Task GetFriend_GetFriends_User(string id)
         {
-            var result =_friendService.GetFriends(id);
+            var friendly = _friendly[id];
+            var users = _friendService.GetFriends(id);
+            var usersId = users.Select(a => a.Id);
             
-            Assert.IsNotNull(result);
-            Assert.IsInstanceOf<IEnumerable<User>>(result);
+            Assert.NotNull(users,"users is null");
+            Assert.NotNull(usersId,"usersId is null");
+            Assert.NotNull(friendly,"friendly is null");
             
+            Assert.IsInstanceOf<IEnumerable<User>>(users);
+            Assert.IsInstanceOf<IEnumerable<string>>(usersId);
+            Assert.IsInstanceOf<IEnumerable<string>>(friendly);
+            
+            Assert.AreEqual(usersId,friendly);
+        }
+        
+        [TestCase("0")]
+        [TestCase("1")]
+        [TestCase("2")]
+        public async Task GetFriendsWithStatus_GetStatusFriends_Users(string id)
+        {
+            var friendly = _friendly[id];
+            Assert.NotNull(friendly,"friendly is null");
+            
+            var usersWithStatus = _friendService.GetFriendsWithStatus(id);
+            Assert.NotNull(usersWithStatus,"users is null");
+
+            var usersId = usersWithStatus.Keys;
+            Assert.NotNull(usersId,"usersId is null");
+            
+            Assert.IsInstanceOf<IEnumerable<string>>(usersId);
+            Assert.IsInstanceOf<IEnumerable<string>>(friendly);
+            Assert.IsInstanceOf<Dictionary<string,bool>>(usersWithStatus);
+            
+            Assert.AreEqual(usersId,friendly);
+
         }
     }
 }
